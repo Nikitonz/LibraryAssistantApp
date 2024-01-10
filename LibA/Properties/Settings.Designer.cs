e2719885dbd9ad100c3cab323c9a5a8b4117d509
Nikitonz <nikitoniy2468@gmail.com>
2024-01-10 11:07:12 +0300

    fix

--- LibA/Properties/Settings.Designer.cs
+++ LibA/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace LibA.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("127.0.0.1")]
         public string dbConnSourceAddr {
             get {
                 return ((string)(this["dbConnSourceAddr"]));
